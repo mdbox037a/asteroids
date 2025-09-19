0797264795a2526542028deafd1420e267dc0728
matt box <mdbox037a@protonmail.com>
2025-09-18 22:53:14 -0400

    add frame rate maximum with clock.tick()

--- main.py
+++ main.py
@@ -5,13 +5,18 @@ from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, "Black")
+
+        screen.fill("black")
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
