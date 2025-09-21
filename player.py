5ffdcfc46309b662ed3040bc32c3c98913def4dd
matt box <mdbox037a@protonmail.com>
2025-09-21 10:29:48 -0400

    create player file skeleton to create player sprite

---
+++ player.py
@@ -0,0 +1,8 @@
+import circleshape
+import constants
+
+
+class Player(circleshape.CircleShape):
+    def __init__(self, x, y):
+        super().__init__(x, y, constants.PLAYER_RADIUS)
+        self.rotation = 0
