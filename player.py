5ff23f5708224c255b3777a80a9ad6409d54a2b2
matt box <mdbox037a@protonmail.com>
2025-09-23 16:00:46 -0400

    implement shots from player sprite

--- player.py
+++ player.py
@@ -1,6 +1,9 @@
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED
+from constants import PLAYER_RADIUS, PLAYER_SHOOT_SPEED, PLAYER_TURN_SPEED, PLAYER_SPEED
+from shot import Shot
 import pygame
+
+import shot
 
 
 class Player(CircleShape):
@@ -37,3 +40,9 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
+
+    def shoot(self):
+        shot = Shot(self.position.x, self.position.y)
+        shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
